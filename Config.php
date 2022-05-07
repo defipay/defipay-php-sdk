f699d37c2178ad9061592c033e4c0d021323a092
harlan <438136451@qq.com>
2022-05-07 16:28:46 +0800

    init

---
+++ Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Defipay\api;
+class Config
+{
+    const PROD = [
+        "host" => "http://api-test.defipay.biz",
+        "defipayPub" => "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4"
+    ];
+    const SANDBOX = [
+        "host" => "http://api-test.defipay.biz",
+        "defipayPub" => "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4"
+    ];
+}
+
+
