f47ddc25a613103920e19c7683d1f0c5eff1ded9
harlan <438136451@qq.com>
2022-05-13 16:30:32 +0800

    测试环境更新

--- Config.php
+++ Config.php
@@ -4,11 +4,11 @@ namespace Defipay\api;
 class Config
 {
     const PROD = [
-        "host" => "http://api-test.defipay.biz",
+        "host" => "https://api.defipay.biz",
         "defipayPub" => "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4"
     ];
     const SANDBOX = [
-        "host" => "http://api-test.defipay.biz",
+        "host" => "https://api-test.defipay.biz",
         "defipayPub" => "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4"
     ];
 }
